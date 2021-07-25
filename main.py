9c9e73eac140914c3315dff2deab1e0a40e41800
Sarah Masoumi <Sarah.Masoomi31@gmail.com>
2021-07-25 15:59:25 +0430

    main corrected

--- main.py
+++ main.py
@@ -4,7 +4,5 @@ d = Database('schema.txt')
 
 t = d._tables['User']
 row = { 'username': 'Sarah Genius', 'password' : 'Amir ham bahooshe', 'joined_at': '2021-12-11' }
-row1 = { 'username': 'Sarah Genius', 'password' : 'asdas', 'joined_at': '2021-12-11' }
 t.addRow(row)
-t.addRow(row1)
 print(t.selectRow())
