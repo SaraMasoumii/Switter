7ff4ffcc66c818c21e9c66109b5fb31b4dc69cdd
Sarah <sarah.masoomi31@gmail.com>
2021-08-09 01:21:46 +0430

    Some functions added to Application

--- application/application.py
+++ application/application.py
@@ -1,7 +1,7 @@
 from typing import List
 
 from database.database import Database
-
+from application.like import Like
 from application.tweet import Tweet
 from application.user import User
 
@@ -27,16 +27,31 @@ class Application:
 		for dic in list:
 			tweet = Tweet(dic)
 			tweets.append(tweet)
-		return tweet
+		return tweets
 
 	def getAllTweets(self) -> List[Tweet]:
-		pass
+		list = self.database.query("SELECT FROM Tweets")
+		tweets = []
+		for dic in list:
+			tweet = Tweet(dic)
+			tweets.append(tweet)
+		return tweets
 
 	def getLikesFromTweet(self, tweet: Tweet) -> List[Like]:
-		pass
+		list = self.database.query(f"SELECT FROM Likes WHERE id={tweet.id}")
+		likes = []
+		for dic in list:
+			like = Like(dic)
+			likes.append(like)
+		return likes
 
 	def getMentionsFromTweet(self, tweet: Tweet) -> List[Tweet]:
-		pass
+		list = self.database.query(f"SELECT FROM Tweets WHERE parID={tweet.parID}")
+		mentions = []
+		for dic in list:
+			mention = Tweet(dic)
+			mentions.append(mention)
+		return mentions
 
 	def tweet(self, text: str):
 		pass
