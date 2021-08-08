69a4f639cd8f58d32b0bf4e334785d5bbc3ff7ac
Sarah <sarah.masoomi31@gmail.com>
2021-08-09 00:31:32 +0430

    Application start

--- application/application.py
+++ application/application.py
@@ -1,5 +1,9 @@
-from application.user import User
+from typing import List
+
 from database.database import Database
+
+from application.tweet import Tweet
+from application.user import User
 
 
 class Application:
@@ -7,6 +11,38 @@ class Application:
 	def __init__(self) -> None:
 		self.database = Database('application/schema.txt')
 
+	def login(self, username: str, password: str) -> None:
+		self.user = User(username, password)
+		pass
+
+	def logout(self) -> None:
+		pass
+
 	def register(self, username: str, password: str) -> None:
-		# ...
+		user = User(username, password)
+		
+	def getTweetsFromUser(self, user: User) -> List[Tweet]:
+		list = self.database.query(f"SELECT FROM Tweets WHERE username={user.username}")
+		tweets = []
+		for dic in list:
+			tweet = Tweet(dic)
+			tweets.append(tweet)
+		return tweet
+
+	def getAllTweets(self) -> List[Tweet]:
+		pass
+
+	def getLikesFromTweet(self, tweet: Tweet) -> List[Like]:
+		pass
+
+	def getMentionsFromTweet(self, tweet: Tweet) -> List[Tweet]:
+		pass
+
+	def tweet(self, text: str):
+		pass
+
+	def mention(self, text: str, parTweet: Tweet):
+		pass
+
+	def retweet(self, tweet: Tweet):
 		pass
--- application/tweet.py
+++ application/tweet.py
@@ -1,8 +1,20 @@
+from typing import Dict
+
+
 class Tweet:
 	
-	def __init__(self, id: str, text: str, author: str, date: str, parID: str = None) -> None:
+	def __init__(self, id: str, text: str, username: str, retweeter: str, date: str, parID: str = None) -> None:
 		self.id = id
 		self.text = text
-		self.author = author
+		self.username = username
+		self.retweeter = retweeter
 		self.date = date
 		self.parID = parID
+
+	def __init__(self, dict: Dict[str, str]) -> None:
+		self.id = dict['id']
+		self.text = dict['text']
+		self.username = dict['username']
+		self.retweeter = dict['retweeter']
+		self.date = dict['date']
+		self.parID = dict['par_id']
