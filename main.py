852a0fb7c7163c8e5b98dd7d80ff8e81435f2840
Sarah <sarah.masoomi31@gmail.com>
2021-08-09 22:41:30 +0430

    showTweets mention & next & prev handled

--- main.py
+++ main.py
@@ -3,77 +3,112 @@ import sys
 from application import *
 from database import *
 
+a = Application()
 
 def clear():
-    os.system('cls||clear')
-
+	os.system('cls||clear')
 
 def printTweet(tweet: Tweet):
-    print(f"Date: {tweet.date}\n")
-    if tweet.retweeter != '0':
-        print(f"ReTweeted by @{tweet.retweeter}:\n")
-    print(f"@{tweet.username}:\n")
-    print(f"{tweet.text}:\n")
+	print(f"Date: {tweet.date}\n")
+	if tweet.retweeter != '0':
+		print(f"ReTweeted by @{tweet.retweeter}:\n")
+	print(f"@{tweet.username}:\n")
+	print(f"{tweet.text}:\n")
 
+def showLikes(tweet: Tweet):
+	likes = a.getLikesFromTweet(tweet)
 
 def showTweets(tweets: List[Tweet]):
-    if len(tweets) == 0:
-        print("Nothing to show here!")
-        return
-    idx = len(tweets) - 1
-    while True:
-        showTweets(tweets[idx])
-
-
+	if len(tweets) == 0:
+		print("Nothing to show here!\n")
+		return
+	idx = len(tweets) - 1
+	while True:
+		clear()
+		tweet = tweets[idx]
+		printTweet(tweet)
+		while True:
+			cm = input("- Choose a command (next/prev/back/like/retweet/mention/showlikes/showmentions): ")
+			if cm == 'next':
+				if idx - 1 >= 0:
+					idx -= 1
+					break
+				else:
+					print("\nYou have reached the end!\n")
+					continue
+			elif cm == 'prev':
+				if idx + 1 < len(tweets):
+					idx += 1
+					break
+				else:
+					print("\nYou have reached the top!\n")
+					continue
+			elif cm == 'back':
+				clear()
+				return
+			elif cm == 'like':
+				pass
+			elif cm == 'retweet':
+				pass
+			elif cm == 'mention':
+				pass
+			elif cm == 'showlikes':
+				pass
+			elif cm == 'showmentions':
+				mentions = a.getMentionsFromTweet(tweet)
+				showTweets(mentions)
 
 def tweet(text: str):
-    a.tweet(text)
-    print('Your tweet is successfully tweeted.')
-
+	a.tweet(text)
+	print('Your tweet is successfully tweeted.')
 
 def mention(text: str, parTweet: Tweet):
-    a.mention(text, parTweet)
-    print('Your mention is successfully mentioned')
-
+	a.mention(text, parTweet)
+	print('Your mention is successfully mentioned')
 
 def retweet(parTweet: Tweet):
-    pass
+	pass
 
 def like():
-    pass
+	pass
 
-
-a = Application()
 clear()
 print('** Welcome to Switter! ** \n')
 print('Created by Sarah Masoumi.\n')
 while not a.isLoggedIn():
-    cm = input("- Choose a command (login/register): ")
-    clear()
-    if cm == 'login':
-        while True:
-            username = input('- Username: ')
-            password = input('- Password: ')
-            try:
-                a.login(username, password)
-                break
-            except:
-                print('\nPlease try again!\n')
-    elif cm == 'register':
-        while True:
-            username = input('- Enter your username: ')
-            password = input('- Enter your password: ')
-            check_password = input('- ReEnter your password: ')
-            if password != check_password:
-                print("\nPasswords don't match!\nPlease try again!\n")
-            else:
-                try:
-                    a.register(username, password)
-                    break
-                except:
-                    print('\nPlease try again!\n')
-    else:
-        print("Invalid Command!\nPlease try again!\n")
-        continue
+	cm = input("- Choose a command (login/register): ")
+	clear()
+	if cm == 'login':
+		while True:
+			username = input('- Username: ')
+			password = input('- Password: ')
+			try:
+				a.login(username, password)
+				break
+			except:
+				print('\nPlease try again!\n')
+	elif cm == 'register':
+		while True:
+			username = input('- Enter your username: ')
+			password = input('- Enter your password: ')
+			check_password = input('- ReEnter your password: ')
+			if password != check_password:
+				print("\nPasswords don't match!\nPlease try again!\n")
+			else:
+				try:
+					a.register(username, password)
+					break
+				except:
+					print('\nPlease try again!\n')
+	else:
+		print("Invalid Command!\nPlease try again!\n")
+		continue
 
-
+while True:
+	cm = input("- Choose a command (timeline/tweet/logut): ")
+	if cm == 'timeline':
+		showTweets(a.getAllTweets())
+	elif cm == 'tweet':
+		pass
+	elif cm == 'logout':
+		a.logout()
